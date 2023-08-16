17bd63fdddc3e15455cead3ed587c203a22c5401
Connor Elsea <connorelsea@Connors-MBP.lan>
2023-08-15 22:46:49 -0500

    updates

--- config-overrides.js
+++ config-overrides.js
@@ -8,12 +8,12 @@ const BundleAnalyzerPlugin =
 
 const { override, addWebpackPlugin } = require("customize-cra");
 
-// module.exports = function override(config, env) {
-//   // ...
-//   config = rewireStyledComponents(config, env);
-//   // ...
-//   return config;
-// };
+module.exports = function override(config, env) {
+  // ...
+  config = rewireStyledComponents(config, env);
+  // ...
+  return config;
+};
 
 // module.export = override(
 //   // addWebpackPlugin(
@@ -35,30 +35,30 @@ const { override, addWebpackPlugin } = require("customize-cra");
 //   addWebpackPlugin(new BundleAnalyzerPlugin({ generateStatsFile: true }))
 // );
 
-module.exports = {
-  // The Webpack config to use when compiling your react app for development or production.
-  webpack: function (config, env) {
-    // ...add your webpack config
+// module.exports = {
+//   // The Webpack config to use when compiling your react app for development or production.
+//   webpack: function (config, env) {
+//     // ...add your webpack config
 
-    config.plugins = [
-      ...config.plugins,
-      new ImageminWebpWebpackPlugin({
-        config: [
-          {
-            test: /\.(jpe?g|png)/,
-            options: {
-              quality: 75,
-            },
-          },
-        ],
-        overrideExtension: true,
-        detailedLogs: true,
-        silent: false,
-        strict: true,
-      }),
-      new BundleAnalyzerPlugin(),
-    ];
+//     config.plugins = [
+//       ...config.plugins,
+//       new ImageminWebpWebpackPlugin({
+//         config: [
+//           {
+//             test: /\.(jpe?g|png)/,
+//             options: {
+//               quality: 75,
+//             },
+//           },
+//         ],
+//         overrideExtension: true,
+//         detailedLogs: true,
+//         silent: false,
+//         strict: true,
+//       }),
+//       // new BundleAnalyzerPlugin(),
+//     ];
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
